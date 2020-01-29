7b50c56dd5aa5acf1edf242c759928df28304512
Dknwhe <cai.mac@hotmail.com>
2020-01-30 00:33:16 +0100

    done 1/3

---
+++ src/main/java/se/ecutb/cai/App.java
@@ -0,0 +1,104 @@
+package se.ecutb.cai;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+// 1.
+//        int[] myIntArray = new int[25];
+//
+//        for (int i= 0; i < myIntArray.length; i++) {
+//            myIntArray[i] = i*10;
+//        }
+//        printArray(myIntArray);
+//    }
+//
+//    public static void printArray (int[] array) {
+//        for(int i=0; i<array.length; i++) {
+//            System.out.println("Element "+ i + "value is: " +array[i]);
+//        }
+
+//2.
+//        int[] myIntegers = getIntegers(5);
+//        for(int i = 0; i < myIntegers.length; i++) {
+//            System.out.println("Element " + i + ", typed value was " + myIntegers[i]);
+//        }
+//        System.out.println("The average is " + getAverage(myIntegers));
+//    }
+//
+//
+//    private static Scanner scanner = new Scanner(System.in);
+//    public static int[] getIntegers(int number) {
+//        System.out.println("Enter " + number + " integer values.\r");
+//        int[] values = new int[number];
+//
+//        for (int i = 0; i < values.length; i++) {
+//            values[i] = scanner.nextInt();
+//        }
+//
+//        return values;
+//
+//    }
+//
+//    public static double getAverage(int[] array) {
+//        int sum = 0;
+//        for(int i = 0; i< array.length; i++) {
+//            sum += array[i];
+//        }
+//        return (double) sum / (double)array.length;
+//    }
+// 3. exercise
+//        int[] myIntegers = getIntegers(5);
+//        int[] sorted = sortIntegers(myIntegers);
+//        printArray(sorted);
+//
+//    }
+//
+//    private static int[] getIntegers(int capacity) {
+//        int[] array = new int[capacity];
+//        System.out.println("Enter  " + capacity + " integer values:\r");
+//        for(int i = 0; i<array.length; i++ ) {
+//            array[i] = scanner.nextInt();
+//        }
+//        return array;
+//    }
+//
+//    public static void  printArray (int[] array) {
+//        for (int i = 0; i < array.length; i++) {
+//            System.out.println("Element " + i + "contents " + array[i]);
+//        }
+//    }
+//
+//    public static int[] sortIntegers(int[] array) {
+////        int[] sortedArray = new int[array.length];
+////        for (int i= 0; i <array.length; i++) {
+////            sortedArray[i] = array[i];
+////        }
+//
+//        int[] sortedArray = Arrays.copyOf(array, array.length);
+//
+//        boolean flag = true;
+//        int temp;
+//        while(flag) {
+//            flag = false;
+//            for(int i = 0;  i< sortedArray.length-1; i++) {
+//                if(sortedArray[i] < sortedArray [i+1]) {
+//                    temp = sortedArray[i];
+//                    sortedArray[i] = sortedArray[i+1];
+//                    sortedArray[i+1] = temp;
+//                    flag = true;
+//                }
+//            }
+//        }
+//        return sortedArray;
+//    }
+//
+//    private static Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
